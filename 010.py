1e79e27def8c20e50e25e3793d14072d216c9d94
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-08 20:04:19 -0300

    Add files via upload

---
+++ 010.py
@@ -0,0 +1,26 @@
+# Level 010
+
+# --------------------
+# -- Usando Looping --
+# --------------------
+
+import random
+
+numero = random.randint(1, 1000)
+acertou = False
+
+while not acertou:
+    palpite = int(input("Escolha um número aleatório entre 1 e 1000: "))
+
+    if numero == palpite:
+        print("Parabéns, suas chances eram mínimas mas você conseguiu!")
+        acertou = True
+    else: 
+        print("Não desista, tente outra vez!")
+
+    # Bônus para o jogador
+
+        if palpite < numero:
+            print("Tente um número maior!")
+        else: 
+            print("Tente um número menor")
