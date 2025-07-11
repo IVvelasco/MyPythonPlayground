37668d90f47edc746f161240256188e9bb860a6b
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-10 21:37:40 -0300

    Add files via upload

---
+++ 012.py
@@ -0,0 +1,13 @@
+# Level 012
+
+# -----------
+# -- Lista --
+# -----------
+
+
+frutas = ["maçã", "banana", "laranja"]
+print(frutas[0])  # Imprime "maçã"
+print(frutas[1])  # Imprime "banana"
+add = input("Adicione uma fruta à lista: ")
+frutas.append(f"{add}") 
+print(frutas) 
