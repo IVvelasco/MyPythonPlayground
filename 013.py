0e216c83733ff30840f9f666d6d3a62ebf8f47c6
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-11 21:58:01 -0300

    Add files via upload

---
+++ 013.py
@@ -0,0 +1,16 @@
+# Level 013
+
+# ------------------------------
+# -- TABUADA COM LIST & RANGE --
+# ------------------------------
+
+print(f"Tabuada do 1: {list(range(0, 11, 1))}")
+print(f"Tabuada do 2: {list(range(0, 22, 2))}")
+print(f"Tabuada do 3: {list(range(0, 33, 3))}")
+print(f"Tabuada do 4: {list(range(0, 44, 4))}")
+print(f"Tabuada do 5: {list(range(0, 55, 5))}")
+print(f"Tabuada do 6: {list(range(0, 66, 6))}")
+print(f"Tabuada do 7: {list(range(0, 77, 7))}")
+print(f"Tabuada do 8: {list(range(0, 88, 8))}")
+print(f"Tabuada do 9: {list(range(0, 99, 9))}")
+print(f"Tabuada do 10: {list(range(0, 110, 10))}")
