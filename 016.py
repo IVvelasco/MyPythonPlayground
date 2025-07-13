579afde328d8bc31e56ec839f9911c0d62bcf685
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-13 10:59:06 -0300

    Add files via upload

---
+++ 016.py
@@ -0,0 +1,42 @@
+# Level 016
+
+#  --------------------------------
+#  -- OPERADORES LÓGICOS + INPUT --
+#  --------------------------------
+
+num1 = float(input("Insira aqui o primeiro número: "))
+num2 = float(input("Insira aqui o segundo número: "))
+
+print("-------------------------")
+print(" RESULTADO DAS OPERAÇÕES")
+print("-------------------------")
+print("")
+
+print("-------------------------------")
+print(f"-- Adição = {num1} + {num2} = {num1 + num2} --")
+print("-------------------------------")
+print("")
+
+print("----------------------------------")
+print(f"-- Subtração = {num1} - {num2} = {num1 + num2} --")
+print("----------------------------------")
+print("")
+
+print("----------------------------------")
+print(f"-- Divisão = {num1} / {num2} = {num1 / num2} --")
+print("----------------------------------")
+print("")
+
+print("----------------------------------------")
+print(f"-- Divisão inteira = {num1} // {num2} = {num1 // num2} --")
+print("----------------------------------------")
+print("")
+
+print("-------------------------------------")
+print(f"-- Módulo(resto) = {num1} % {num2} = {num1 % num2} --")
+print("-------------------------------------")
+print("")
+
+print("--------------------------------------")
+print(f"-- Exponenciação = {num1} // {num2} = {num1 // num2} --")
+print("--------------------------------------")
