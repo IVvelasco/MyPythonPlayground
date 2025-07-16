293c63cae94b45b08c12edfa94f6af16bc11750d
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-16 10:56:29 -0300

    Add files via upload

---
+++ 030.py
@@ -0,0 +1,12 @@
+# Level 030
+
+#  -------------------------------
+#  -- Boate para maiores de 18  --
+#  -------------------------------
+
+
+idade = int(input("Coloque aqui sua idade: "))
+if idade >= 18:
+    print("Você está liberado para entrar")
+else:
+    print("Você precisa ter 18 anos para entrar nesse local")
