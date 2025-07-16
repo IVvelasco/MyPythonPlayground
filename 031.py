a3adc830c3e2cc9b331b749953d5ad5c55d6340d
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-16 10:56:54 -0300

    Add files via upload

---
+++ 031.py
@@ -0,0 +1,19 @@
+ #Level 031
+
+#  ---------------------------------
+#  -- Questionário ovo ou galinha --
+#  ---------------------------------
+
+print("Quem veio primeiro: o ovo ou a galinha?")
+print("a) Galinha")
+print("b) Ovo")
+resposta = input("Sua resposta: ")
+if resposta == 'a':
+    print("E os dinossauros, hein?")
+elif resposta == 'b':
+    print("Parabéns!")
+else:
+    print("Por favor, insira uma resposta válida")
+     
+
+    
