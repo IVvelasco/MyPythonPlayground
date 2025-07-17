8ef6897bae8e7649532f626089490b1e26bead79
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-17 10:39:13 -0300

    Add files via upload

---
+++ 037.py
@@ -0,0 +1,23 @@
+# Level 037
+
+#  -----------------------------------------
+#  -- TEMPERATURA EM CELSIUS E FAHRENHEIT --
+#  -----------------------------------------
+
+unidade = input("Essa teperatura será em Celsius ou Fahrnheit? (F/C): ")
+temperatura = float(input("Insira a temperatura: "))
+
+if unidade == "C":
+    conversor = round((9 * temperatura) / 5 + 32)
+    print(f"A temperatura de {temperatura} graus Celsius corresponde a {conversor} graus Fahrnheit")
+elif unidade == "c":
+    conversor = round((9 * temperatura) / 5 + 32)
+    print(f"A temperatura de {temperatura} graus Celsius corresponde a {conversor} graus Fahrnheit")
+elif unidade == "F":
+    conversor_c = ((temperatura - 32) * 5 / 9)
+    print(f"A temperatura de {temperatura} graus Fahrnheit corresponde a {conversor_c} graus Celsius")
+elif unidade == "f":
+    conversor_c = ((temperatura - 32) * 5 / 9)
+    print(f"A temperatura de {temperatura} graus Fahrnheit corresponde a {conversor_c} graus Celsius")
+else:
+    print("Esse não é um valor válido, por favor escolha entre F ou C ")
