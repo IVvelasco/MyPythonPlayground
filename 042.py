f88fa1b1214724bbdc05f84c5b79828c999e1597
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-20 19:50:13 -0300

    Add files via upload

---
+++ 042.py
@@ -0,0 +1,15 @@
+
+# Level 042
+
+#  ------------------------------------
+#  -- DECLARAÇÃO EM LOOPING INFINITO --
+#  ------------------------------------
+
+# Esse programa manda beijos infinitos para a pessoa amada! 
+
+name = input("Input here the name of your your love")
+love = True 
+while love == True:
+    print(f"{name}: Kisses")
+else:
+    print("run")
