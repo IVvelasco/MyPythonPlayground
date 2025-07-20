cfae6feb19010ae99188a47fcab9af9670e01fa2
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-20 20:03:31 -0300

    Brincando com Palíndromos

--- 008.py
+++ 008.py
@@ -1,13 +1,13 @@
 # Level 008
 
-# ---------------------------
-# -- Brincando com strings --
-# ---------------------------
+# -------------------------------
+# -- BRINCANDO COM PALÍNDROMOS --
+# -------------------------------
 
 
 """
 Uma maneira simples e eficiente de testar se uma frase ou palavra trata-se de um palíndromo: comparar a string ::+1 com ::-1 e ver se são correspondentes
-
+Observação: palíndromo trata-se de uma palavra que pode ser lida tanto da esquerda para a direita quanto da direita para a esquerda. Exemplo: Arara
 """
 
 frase = input("Será um palíndromo? Digite e descubra: ")
