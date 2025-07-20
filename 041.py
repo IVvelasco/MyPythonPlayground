dcdca86bd569a979f8c42c47c9b23b1612325666
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-20 19:48:51 -0300

    Add files via upload

---
+++ 041.py
@@ -0,0 +1,16 @@
+# Level 041
+
+#  -------------------
+#  -- CARA OU COROA --
+#  -------------------
+
+# Tá em dúvida do que fazer? Jogue essa "moeda" virtual e descubra
+
+import random
+def main():
+
+    moeda = ["Cara", "Coroa"]
+    print(random.choice(moeda))
+
+if __name__ == "__main__":
+    main()
