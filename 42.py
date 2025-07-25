0060b6a83884e2fa9cd855fb83869eadd0139ebe
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-24 22:59:14 -0300

    Add files via upload

---
+++ 42.py
@@ -0,0 +1,19 @@
+# LEVEL 042
+
+# ----------------------
+# -- MEME DO DICAPRIO --
+# ----------------------
+
+# AVISO: Esse algoritmo é feito com base em memes e boatos da internet, sem intenção de ofender ou julgar ninguém. 
+
+print("###############################################################################################")
+print("# Será que você teria idade suficiente para namorar com o Leonardo DiCaprio? Venha e descubra #")
+print("###############################################################################################")
+
+idade = int(input("Coloque aqui sua idade: "))
+if idade >= 25:
+    print("Sinto muito mas você passou da idade :c")
+elif idade <= 17:
+    print("Você é menor de idade, tá fazendo o quê aqui?")
+else:
+    print("Parabéns, você foi selecionada para uma tentativa.")
