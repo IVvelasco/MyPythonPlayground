226fcb5ee4e852da4dc73657e8319b911b82aa04
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-07-25 21:36:29 -0300

    Operdores Ternários - Login

---
+++ 045.py
@@ -0,0 +1,16 @@
+# Level 045
+
+# -----------------------------------
+# -- TESTANDO OPERADORES TERNÁRIOS --
+# --       USER LOGIN OR NOT       --
+# -----------------------------------
+
+def main():
+
+    user_login = True 
+    if user_login == True:
+        print("Você foi loggado com sucesso!")
+    else:
+        print("Nome de Usuário ou Senha incorretos, tente outra vez!")
+if __name__ == "__main__":
+    main()
