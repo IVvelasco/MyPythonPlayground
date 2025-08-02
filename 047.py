7f19925369ac5cf54b9d869032a10a345602a9bb
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-02 13:22:05 -0300

    Desenhando no Terminal

---
+++ 047.py
@@ -0,0 +1,16 @@
+# Level 047
+
+# ----------------------------
+# -- DESENHANDO NO TERMINAL --
+# ----------------------------
+
+# Usar print linha, mesmo com pontuações para substituírem os espaços, faz com que tudo saia embaralhado
+# A solução foi transformar cada linha em um print único e usar pontos como espaços só por garantia
+# Mas testes apontam que o espaço é respeitado quando printa-se linha por linha
+
+print("#####..........#....#....####....#.......###......###......###......####.")
+print("#..............#....#....#.......#......#...#.....#.......#...#....#....#") 
+print("####...........#....#....###.....#......#####.......#.....#........#....#") 
+print("#..............#....#....#.......#......#...#....#...#....#........#....#")
+print("#####....#.......##......####....####...#...#.....####.....###......####.")
+
