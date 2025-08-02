25b596130143c7679e43538b02dc54f5607a4c25
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-02 13:23:52 -0300

    CPF e Booleano

---
+++ 049.py
@@ -0,0 +1,11 @@
+# Level 049
+
+# --------------------
+# -- CPF & BOOLEANO --
+# --------------------
+
+cpf = True
+if cpf == True:
+    print("Seu CPF está negativado")
+else: 
+    print("Nada para ver aqui")
