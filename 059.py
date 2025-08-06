240d30641cee6db360b03527933da8f1294608d5
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-06 18:18:01 -0300

    Quadrado e Cubo

---
+++ 059.py
@@ -0,0 +1,10 @@
+# Level 059
+
+# ---------------------------------
+# -- NÚMERO, SEU QUADRADO E CUBO --
+# ---------------------------------
+
+numero = int(input("Insira um número inteiro: "))
+quadrado = numero ** 2
+cubo = numero ** 3
+print(f"O quadrado de {numero} é {quadrado} e seu cubo é {cubo}")
