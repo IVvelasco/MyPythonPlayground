6e856891aceebf70284b0b1fd2eda1d24a024a64
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-06 18:17:25 -0300

    Intervalo entre 100 e 200

---
+++ 058.py
@@ -0,0 +1,14 @@
+# Level 058
+
+# -------------------------------
+# -- INTERVALO ENTRE 100 E 200 --
+# -------------------------------
+
+numero = int(input("Insira um número inteiro: "))
+
+if numero > 100 < 200:
+    print("Esse número está no intervalo entre 100 e 200")
+else: 
+    print("Esse número não está no intervalo entre 100 e 200")
+
+
