f5ee53920f3519a317c015e2ea0c1304031882e5
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-24 13:05:39 -0300

    Missing Looping

---
+++ 062.py
@@ -0,0 +1,13 @@
+# Level 062
+
+# ---------------------
+# -- MISSING LOOPING --
+# ---------------------
+
+""" 
+A silly code for when you lose a dear friend 
+"""
+
+Missing = True
+while Missing == True:
+  print("I miss you") 
