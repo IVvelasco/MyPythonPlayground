7075971e899363253acaaa3fca87a7234e950b25
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-08-24 15:38:31 -0300

    Funcionários para aposentar-se

--- 062.py
+++ 062.py
@@ -1,13 +1,24 @@
 # Level 062
 
-# ---------------------
-# -- MISSING LOOPING --
-# ---------------------
+# ------------------------------------
+# -- FUNCIONÁRIOS PARA APOSENTAR-SE --
+# ------------------------------------
 
-""" 
-A silly code for when you lose a dear friend 
+"""
+Análise simples de aposentadoria, partindo do princípio de que os funcionários irão aposentar-se por idade e não por contribuição
 """
 
-Missing = True
-while Missing == True:
-  print("I miss you") 
+class funcionario: 
+    def __init__(self, nome, sobrenome, idade, sexo, cpf):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.sexo = sexo
+        self.cpf = cpf 
+        self.idade = idade
+
+        if idade >= '60':
+            print(f"O funcionário {nome} está para aposentar-se")
+        
+if __name__ == "__main__":
+    pessoa0 = funcionario("João", "da Silva","M","000.000.000-00", '60')
+    pessoa1 = funcionario("Roberto", "Costa", "M", "000.000.000-00", '64')
