07ecd64235962d55259c9c3d866543bf11fbc070
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-09-16 22:15:17 -0300

    Par ou ímpar?

---
+++ 070.py
@@ -0,0 +1,12 @@
+# Level 070
+
+# -------------------
+# -- PAR OU ÌMPAR? --
+# -------------------
+
+numero = 70
+resultado = numero % 2
+if resultado == 0:
+    print("Esse número é par")
+else: 
+    print("Esse número é ímpar.")
