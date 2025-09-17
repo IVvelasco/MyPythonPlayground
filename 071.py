af09868bde9f131fabff35ef8e7cd992f23bf44e
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-09-16 22:16:01 -0300

    Par ou ímpar? 2

---
+++ 071.py
@@ -0,0 +1,12 @@
+# Level 071
+
+# -------------------
+# -- PAR OU ÌMPAR? --
+# -------------------
+
+numero = int(input("Insira um número inteiro: "))
+resultado = numero % 2
+if resultado == 0:
+    print("Esse número é par")
+else: 
+    print("Esse número é ímpar.")
