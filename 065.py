8c2bde472fff161b76e478c517b8a3efc43456c2
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-09-16 22:09:20 -0300

    Cifra de César

---
+++ 065.py
@@ -0,0 +1,8 @@
+# Level 065
+
+# --------------------
+# -- CIFRA DE CÉSAR --
+# --------------------
+
+frase = input('Escreva aqui a frase a ser criptografada com a versão moderna da cifra de César: ')
+print(frase [::-1])
