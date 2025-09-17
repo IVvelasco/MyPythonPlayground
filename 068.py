2c8987b601faa88ddb12c6f5e02a099157153ffb
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-09-16 22:13:25 -0300

    ex

---
+++ 068.py
@@ -0,0 +1,12 @@
+# Level 068
+
+# --------
+# -- EX --
+# --------
+
+"""
+Exercício original retirado do livro 'Python for Dummies', 
+"""
+
+ex = input('Escreva o nome do seu ex: ')
+print(f"Hey {ex}, você está no passado!")
