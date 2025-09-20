a96cb7483a7a385485bf86372f99efc04439a2d2
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-09-20 19:32:25 -0300

    A mesa está quente?

---
+++ 072.py
@@ -0,0 +1,10 @@
+#Level 072
+
+# -------------------------
+# -- A MESA ESTÁ QUENTE? --
+# -------------------------
+""" Exercício retirado do livro "Use a cabeça!- C" dos autores David Griffiths e Dawn Griffits, adaptado para Python """
+
+card_count = 11
+if card_count > 10:
+    print("A mesa está quente, pode jogar")
