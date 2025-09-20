90aab1bb0f5954855a4ed16534237abcc4430264
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-09-20 19:33:50 -0300

    Cartas 2

---
+++ 074.py
@@ -0,0 +1,18 @@
+# Level 074
+
+# -------------
+# -- CARTAS 2--
+# -------------
+
+carta = input('Insira uma letra e verifique o Naipe: ')
+
+if carta == 'E':
+    print("Espadas")
+elif carta == 'C':
+    print("Copas")
+elif carta == 'T':
+    print("Trevos")
+elif carta == 'P':
+    print("Paus")
+else:
+    print("Insira um naipe válido!")
