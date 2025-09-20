ee9cedfbea0f8a1b5c3f67b4665ec9d072f83c8f
Velasco <106848764+IVvelasco@users.noreply.github.com>
2025-09-20 19:33:13 -0300

    Cartas

---
+++ 073.py
@@ -0,0 +1,18 @@
+# Level 073
+
+# ------------
+# -- CARTAS --
+# ------------
+
+""" Exercício retirado do livro "Use a cabeça!- C" dos autores David Griffiths e Dawn Griffits, adaptado para Python """
+
+carta = 'C'
+
+if carta == 'E':
+    print("Espadas")
+elif carta == 'C':
+    print("Copas")
+elif carta == 'T':
+    print("Trevos")
+elif carta == 'P':
+    print("Paus")
