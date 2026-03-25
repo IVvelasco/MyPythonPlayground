2e434d09b45fab7faead0cd2fc4511036b9289ea
Velasco <106848764+IVvelasco@users.noreply.github.com>
2026-03-25 06:54:26 -0300

    Tkinter

---
+++ 085.py
@@ -0,0 +1,14 @@
+# 085
+
+# -------------
+# -- Tkinter --
+# -------------
+
+# How to create a window in Tkinter using only 4 lines of code
+
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+
+window.mainloop()
